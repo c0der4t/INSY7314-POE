13ebcc746d000ad9d1cf3c3fdb88c89877615186
Chris Bartie <st10033223@vcconnect.edu.za>
2025-10-06 14:02:24 +0200

    swift code regex

--- backend/middlewares/validateInputs.js
+++ backend/middlewares/validateInputs.js
@@ -30,7 +30,13 @@ const validateInputs = (req, res, next) => {
     const accNumRegex = /^([0-9]{11})|([0-9]{2}-[0-9]{3}-[0-9]{6})$/; //11 digits for the acc number and can be seperated by a hyphen
     const amountRegex = ;
     const currencyRegex = ;
-    const swiftCodeRegex = ;
+
+    //Code Attribution
+    //This regex pattern for the SWIFT code was taken from StackOverflow
+    //https://stackoverflow.com/questions/3028150/what-is-proper-regex-expression-for-swift-codes
+    //Klesun
+    //https://stackoverflow.com/users/2750743/klesun
+    const swiftCodeRegex = /[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?/i;
 
 
 
@@ -41,3 +47,4 @@ const validateInputs = (req, res, next) => {
 //https://stackoverflow.com/questions/29383955/how-to-write-a-regex-javascript-for-an-id-validation
 //https://stackoverflow.com/questions/9628879/javascript-regex-username-validation
 //https://stackoverflow.com/questions/22749891/regex-validate-an-account-number-with-two-different-patterns
+//https://stackoverflow.com/questions/3028150/what-is-proper-regex-expression-for-swift-codes
