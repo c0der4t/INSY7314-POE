247b3024931edc31f5cd47d5876fb50c6f7748ef
Chris Bartie <st10033223@vcconnect.edu.za>
2025-10-06 08:22:58 +0200

    authController and userModel updated
    authController now contained a try catch block for login checks and user model takes in accNum and idNum

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 // define the blueprint of a user
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true}
+    username: { type: String, required: true },
+    idNum: { type: String, required: true },
+    accNum: { type: String, required: true },
+    password: { type: String, required: true }
 });
 
 //link it to the db
