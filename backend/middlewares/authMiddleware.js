cb588103b6d26098ff93817b9b77710b3a3a6d08
Chris Bartie <st10033223@vcconnect.edu.za>
2025-10-06 08:34:48 +0200

    set up input validation, need to implement regex patterns

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,18 +1,23 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').configure();
 
+//blacklist of tokens that we have invalidated
 const tokenBlacklist = new Set();
 
 const verifyToken = (req, res, next) => {
-
+    //stripping the header and getting the auth field from header
     const authHeader = req.headers['authorization'];
+    //split after space
     const token = authHeader && authHeader.split(' ')[1];
+
+    //if no token = 401 unauthorized
     if (!token) return res.status(401).json({ message: 'Access Denied: No Token Provided' });
-    if (tokenBlacklist.has(token)) return res.status(401).json({ message: 'Access Denied: Token has been revoked' });
+    //if the token is in our blacklist, 401 unauthorized
+    if (tokenBlacklist.has(token)) return res.status(401).json({ message: 'Access Denied: Token has been invalidated' });
 
-
+    //verify the token
     jwt.verify(token, process.env.JWT_SECRET, (err) => {
-        
+        //403 - not allowed
         if (err) return res.status(403).json({ message: 'Invalid Token' });
         next();
     });
---
+++ backend/middlewares/validateInputs.js
@@ -0,0 +1,15 @@
+const validateInputs = (req, res, next) => {
+    const { username, password, idNum, accNum, amount, currency, swiftCode } = req.body;
+
+    //regex patterns for input whitelisting (need to still do some research on these to implement)
+    const usernameRegex = ;
+    const passwordRegex = ;
+    const idNumRegex = ;
+    const accNumRegex = ;
+    const amountRegex = ;
+    const currencyRegex = ;
+    const swiftCodeRegex = ;
+
+    
+
+}
