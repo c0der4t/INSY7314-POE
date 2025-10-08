bdeee16c0867c5a7b9a8a5a81dfea378f80c6785
Hannah <126674584+Diana-Magus@users.noreply.github.com>
2025-10-08 15:37:04 +0200

    screens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login.jsx';
+import Signup from './pages/Signup/Signup.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Login />} />       
+        <Route path="/login" element={<Login />} />  
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Signup/Signup.jsx
@@ -0,0 +1,44 @@
+import './Signup.css';
+import piggyBank from '../../assets/Images/piggy-bank.png'; 
+import { Link } from 'react-router-dom';
+
+export default function Signup() {
+  return (
+    <div className="container">
+
+      <h1 className="heading">Stoinks</h1>
+      
+            <img src={piggyBank} alt="Piggy Bank" />
+      
+      <h2 className="sub-heading">Signup</h2>
+
+
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        placeholder="Enter your username"
+      />
+
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Enter your password"
+      />
+
+      <button>Signup</button>
+
+      <button>
+        <Link to="/login" style={{ color: 'white', textDecoration: 'none' }}>
+            Login
+        </Link>
+        </button>
+
+
+      
+    </div>
+  );
+}
